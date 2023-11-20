af58a0ce47e686e41b85f3dea452c1c5c0c573be
SpicyCiruit <simon.r.swopes@gmail.com>
2023-11-20 13:30:25 -0600

    early home screen

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,13 +1,39 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Pressable} from 'react-native';
 
 // Local Imports
 import {styles} from '../utils/styleSheet.js';
 
+
+/*TODO: Once screens are build import them and replace console.log with naviagtion.navigate */
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-        <Text style={styles.subTitle}>Home</Text>
+        <Text style={styles.title}>Welcome</Text>
+        <View style={styles.spacer}/>
+        <View style={styles.rowContainer}>
+          <Pressable style={styles.button} onPress={() => console.log('Clicked')}>
+            <Text style={styles.buttonText}>Browse Tickets</Text>
+          </Pressable>
+          <View style={styles.spacer}/>
+          <Pressable style={styles.button} onPress={() => console.log('Clicked')}>
+            <Text style={styles.buttonText}>Previous Winners</Text>
+          </Pressable>
+        </View>
+        <View style={styles.spacer}/>
+        <View style={styles.rowContainer}>
+          <Pressable style={styles.button} onPress={() => console.log('Clicked')}>
+            <Text style={styles.buttonText}>Order History</Text>
+          </Pressable>
+          <View style={styles.spacer}/>
+          <Pressable style={styles.button} onPress={() => console.log('Clicked')}>
+            <Text style={styles.buttonText}>Search Tickets</Text>
+          </Pressable>
+        </View>
+        <View style={styles.spacer}/>
+        <Pressable style={styles.button} onPress={() => console.log('Clicked')}>
+          <Text style={styles.buttonText}>Profile</Text>
+        </Pressable>
     </View>
   );
 };
