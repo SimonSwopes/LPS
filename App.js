d4baf1bb89a16d835c3c5a0de0c62d60cba2a1bf
SpicyCiruit <simon.r.swopes@gmail.com>
2023-11-20 12:35:37 -0600

    wrong push

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, ScrollView, Button } from 'react-native';
+import 'react-native-gesture-handler';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+// Local Imports
+import {styles} from './src/utils/styleSheet.js';
+
+// Screen Imports
+import HomeScreen from './src/screens/home.js';
+import LoginScreen from './src/screens/login.js';
+
+// Global Navigation Stack
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello, World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator //header formating
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#0096FF'
+          },
+          headerTintColor: '#000',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            color: '#fff',
+            fontFamily: 'Times New Roman',
+          },
+        }}
+      >
+      <Stack.Screen name="Login" component={LoginScreen} options={{title:'Login'}}/>
+      <Stack.Screen name="Home" component={HomeScreen} options={{title: 'Home'}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/utils/styleSheet.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  subContainer: {
+    flex: 2,
+    justifyContent: 'center',
+    width: "80%",
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+
+  title: {
+    color: '#000',
+    fontFamily: 'Times New Roman',
+    fontSize: 48,
+    fontWeight: 'bold',
+  },
+
+  subTitle: {
+    color: '#000',
+    fontFamily: 'Times New Roman',
+    fontSize: 24,
+  },
+  
+  inputView: {
+    marginTop: 40,
+    width: "80%",
+    borderColor: '#000',
+    borderRadius: 25,
+    height:50,
+    justifyContent: 'center',
+    padding: 20,
+    borderWidth: 1,
+    alignItems: 'center',
+  },
+
+  inputText:{
+    height:50,
+    color:'#000',
+  },
+});
