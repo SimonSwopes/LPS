ebf6adeb1a297be9481761567ea205f00605b6da
SpicyCiruit <simon.r.swopes@gmail.com>
2023-11-29 12:15:53 -0600

    imports for admin screens

--- App.js
+++ App.js
@@ -19,6 +19,8 @@ import ProfileScreen from './src/screens/profile.js';
 import SearchScreen from './src/screens/search.js';
 import SignUpScreen from './src/screens/signup.js';
 import AdminHomeScreen from './src/screens/adminHome.js';
+import StatusScreen from './src/screens/status.js';
+import ManageScreen from './src/screens/manage.js';
 
 // Global Navigation Stack
 const Stack = createStackNavigator();
@@ -66,6 +68,8 @@ export default function App() {
       <Stack.Screen name="Search" component={SearchScreen} options={{title: 'Search Tickets'}}/>
       <Stack.Screen name="SignUp" component={SignUpScreen} options={{title: 'Register'}}/>
       <Stack.Screen name="AdminHome" component={AdminHomeScreen} options={{title: 'Admin Home'}}/>
+      <Stack.Screen name="Status" component={StatusScreen} options={{title: 'System Status'}}/>
+      <Stack.Screen name="Manage" component={ManageScreen} options={{title: 'Manage Tickets'}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
