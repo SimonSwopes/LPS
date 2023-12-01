54617968ba82c1e13b1508dc56e4515b6d03a1f9
SpicyCiruit <simon.r.swopes@gmail.com>
2023-12-01 12:23:36 -0600

    search fucntionality

--- src/screens/search.js
+++ src/screens/search.js
@@ -1,12 +1,53 @@
-import React from 'react';
-import {Text, View} from 'react-native';
-
+import React, {useState} from 'react';
+import {Text, View, TextInput, Button} from 'react-native';
+import * as SQLite from 'expo-sqlite';
 import {styles} from '../utils/styleSheet.js';
 
+const ticketDB = SQLite.openDatabase('ticketData.db');
+
 const SearchScreen= ({ navigation}) => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResult, setSearchResult] = useState(null);
+
+  const handleSearch = () => {
+    ticketDB.transaction((tx) => {
+      tx.executeSql(
+        'SELECT * FROM tickets WHERE type LIKE ?',
+        [`%${searchTerm}%`],
+        (_, { rows }) => {
+          setSearchResult(rows._array);
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+    });
+  };
   return (
     <View style={styles.container}>
-      <Text style={styles.subTitle}>Search</Text>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          inputMode='search'
+          placeholder='Search'
+          autoFocus = {true}
+          defaultValue = '*'
+          value={searchTerm}
+          onChangeText={(text) => setSearchTerm(text)}
+          onSubmitEditing={handleSearch}
+        />
+      </View>
+      {searchResult && (
+        <View style={styles.subFlex}>
+          {searchResult.map((item) => (
+            <Button
+              key={item.id}
+              title={item.type}
+              onPress={() => console.log('Proceed to checkout')}
+            />
+          ))}
+        </View>
+      )}
     </View>
   );
 }
